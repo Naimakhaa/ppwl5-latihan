b1972fd96fa1f7c1fef905cecc1d630dbd77dd46
kharizma <h1101241050@student.untan.ac.id>
2026-03-04 14:40:56 +0700

    pre-filter cleanup

---
+++ src/config/db.ts
@@ -0,0 +1,15 @@
+import { Database } from "bun:sqlite";
+import { env } from "./env";
+
+export const db = new Database(env.DB_FILE);
+
+export const initDB = () => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            role TEXT
+        )
+    `);
+    console.log("📁 Database initialized");
+};
